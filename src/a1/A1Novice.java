da404b532f831a6aa6d54ceb10adbb81bd13a5fa
yoonhosung <yoonhosung@192.168.1.4>
2019-08-28 16:36:57 -0400

    Check to see if it works

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,30 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numCus = scan.nextInt();
+		
+		for (int i = 0; i < numCus; i++)
+		{
+			
+			String firstName = scan.next();
+			String[] splitFirst = firstName.split("");
+			String lastName = scan.next();
+			
+			int itemNum = scan.nextInt();
+			double totalPrice = 0;
+			
+				for (int j = 0; j < itemNum; j++)
+				{
+					int quantity = scan.nextInt();
+					String itemName = scan.next();
+					double itemPrice = scan.nextDouble();
+					double eachPrice = itemPrice * quantity;
+					totalPrice += eachPrice;
+				}
+	
+			System.out.println(splitFirst[0] + "." + " " + lastName + ":" + " " + totalPrice);	
+			
+		}
 		
 	}
 }
