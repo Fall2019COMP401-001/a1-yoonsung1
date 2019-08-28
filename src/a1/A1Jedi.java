3030056c1c76d2b41edd6cb12ad888a95f7896ba
yoonhosung <yoonhosung@192.168.1.4>
2019-08-28 18:37:24 -0400

    Check to see if Jedi works correctly

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,7 @@
 package a1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -9,6 +11,68 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
+		int itemNum = scan.nextInt();
+		ArrayList<String> itemList = new ArrayList<String> ();
+		ArrayList<Double> priceList = new ArrayList<Double> ();
+		
+		for (int i = 0; i < itemNum; i++)
+		{
+			String itemName = scan.next();
+			itemList.add(itemName);
+			double itemPrice = scan.nextDouble();
+			priceList.add(itemPrice);
+			//priceList.add(String.format("%.2f", itemPrice));
+		}
+		
+		int cusNum = scan.nextInt();
+		
+		ArrayList<String> firstList = new ArrayList<String> ();
+		ArrayList<String> lastList = new ArrayList<String> ();
+		ArrayList<Double> totalList = new ArrayList<Double> ();
+		//ArrayList<Integer> quanList = new ArrayList<Integer> ();
+		
+		int array[] = new int[itemList.size()];
+		for (int i = 0; i < itemList.size(); i++)
+		{
+			array[i] = 0;
+		}
+		
+		for (int i = 0; i < cusNum; i++)
+		{
+			String firstName = scan.next();
+			firstList.add(firstName);
+			String lastName = scan.next();
+			lastList.add(lastName);
+			
+			int buyNum = scan.nextInt();
+			double totalPrice = 0;
+			
+				for (int j = 0; j < buyNum; j++)
+				{
+					int quantity = scan.nextInt();
+					String itsName = scan.next();
+					totalPrice += quantity * priceList.get(itemList.indexOf(itsName));
+					
+					int itemIndx = itemList.indexOf(itsName);
+					//int addedValue = quantity + quanList.get(itemIndx);
+					array[itemIndx] += quantity;
+				}
+			
+				totalList.add(totalPrice);
+		}
+		
+		for (int c = 0; c < array.length; c++)
+		{
+			if (array[c] != 0)
+				{
+					System.out.println(array[c] + " customers bought " + itemList.get(c));
+				}
+			else
+				{
+					System.out.println("No customers bought " + itemList.get(c));
+				}
+		}
 		
 	}
 }
